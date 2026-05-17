9d695bf34c2de8bd3025e000bd0b6c9d605056ab
Abhay Keshari <instantboostbyabhaykeshari@gmail.com>
2026-05-17 10:15:50 +0530

    Server code almost completed with all the required features and start the client

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Task Manager Frontend
+      </h1>
+    </div>
+  );
+}
+
+export default App;
